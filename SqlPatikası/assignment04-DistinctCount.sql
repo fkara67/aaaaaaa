6ab3bbce3ebc6c2be5050ad965bfb9d189761e8a
Fatih Kara <fkara1667@gmail.com>
2023-11-10 10:27:51 +0300

    Distinct and Count Assignment added

--- SqlPatikası/assignment04-DistinctCount.sql
+++ SqlPatikası/assignment04-DistinctCount.sql
@@ -11,3 +11,7 @@ WHERE title LIKE 'T%' AND rating = 'G';
 --country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
 SELECT COUNT(*) FROM country
 WHERE country LIKE '_____';
+
+--city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+SELECT COUNT(*) FROM city
+WHERE city LIKE 'R%r';
