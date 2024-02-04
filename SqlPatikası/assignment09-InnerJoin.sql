604be82b65b5528ff1f8d1db991c11d8e96f5bb3
fatihkara67 <fatih.kara@efectura.com>
2024-02-04 21:16:01 +0300

    assignment about inner join

--- SqlPatikası/assignment09-InnerJoin.sql
+++ SqlPatikası/assignment09-InnerJoin.sql
@@ -10,3 +10,5 @@ INNER JOIN payment ON customer.customer_id = payment.customer_id;
 --customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
 SELECT rental_id, first_name, last_name FROM customer
 INNER JOIN rental ON customer.customer_id = rental.customer_id;
+
+--Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
