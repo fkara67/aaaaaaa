7c3dc362ae413fd72da4cb2245929f7f32639224
Fatih Kara <111566864+fkara67@users.noreply.github.com>
2024-02-16 23:21:13 +0300

    Update assignment11-UnionIntersectExcept.sql

--- SqlPatikası/assignment11-UnionIntersectExcept.sql
+++ SqlPatikası/assignment11-UnionIntersectExcept.sql
@@ -12,3 +12,8 @@ SELECT first_name FROM customer;
 SELECT first_name FROM actor
 EXCEPT
 SELECT first_name FROM customer;
+
+--Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT name FROM students
+WHERE marks > 75
+ORDER BY RIGHT(name, 3), id;
